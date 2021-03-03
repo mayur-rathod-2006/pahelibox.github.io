15a85e8e269e8a17657e33e88cfe9b8957c71c9e
Mayur Rathod <mayurhirachandrathod@gmail.com>
2021-03-03 17:49:06 +0530

    I am publishing my 1st website
    Wow , I am happy

---
+++ questions.js
@@ -0,0 +1,129 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which is the largest country in the world?",
+    answer: "Russia",
+    options: [
+      "India",
+      "Russia",
+      "America",
+      "England"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which is the hottest planet in the solar system?",
+    answer: "Venus",
+    options: [
+      "Mars",
+      "Mercury",
+      "Venus",
+      "Earth"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which is the most populated country in the world?",
+    answer: "China",
+    options: [
+      "China",
+      "India",
+      "Australia",
+      "Mexico"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which is the longest river in the world?",
+    answer: "Nile river",
+    options: [
+      "Brahmaputra river",
+      "Narmada river",
+      "Ganga river",
+      "Nile river"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Whis is the world's smallest country?",
+    answer: "Vatican city",
+    options: [
+      "Vatican city",
+      "pune city",
+      "Sydney city",
+      "Mumbai city"
+    ]
+  },
+{
+    numb: 6,
+    question: "When is international womens day?",
+    answer: "8th march",
+    options: [
+      "31st october",
+      "11th february",
+      "8th march",
+      "21st may"
+    ]
+  },  
+  {
+    numb: 7,
+    question: "Which is the biggest animal in the world?",
+    answer: "Blue whale",
+    options: [
+      "Blue whale",
+      "Elephant",
+      "Camel",
+      "Giraffe"
+    ]
+  },
+  {
+    numb: 8,
+    question: "In which country is the Taj Mahal located?",
+    answer: "India",
+    options: [
+      "America",
+      "Pakistan",
+      "Japan",
+      "India"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Who is the father of computer?",
+    answer: "Charles babbage",
+    options: [
+      "Albert einstin",
+      "Charles babbage",
+      "Homi bhabha",
+      "A.R.Rahman"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Who invented the bulb?",
+    answer: "Thomas edison",
+    options: [
+      "Charles darwin",
+      "Isaac newton",
+      "Marie curie",
+      "Thomas edison"
+    ]
+  },
+
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
